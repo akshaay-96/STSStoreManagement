5482b835183402255d94689f01c5698898d94ec4
akshaay-96 <chavanap96@gmail.com>
2025-03-08 21:45:13 +0530

    Adding get API

--- StoreManagement/src/main/java/com/demo/controller/ProductController.java
+++ StoreManagement/src/main/java/com/demo/controller/ProductController.java
@@ -1,9 +1,19 @@
 package com.demo.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import com.demo.model.Product;
 
 @RestController 
 public class ProductController {
 
+	@GetMapping("get_product")
+	public List<Product> getAllProduct()
+	{
+		return new ArrayList<>();
+	}
 	 
 }
