0137c7c58736ee9e0fbfbdf54c17d3c55dc88d58
akshaay-96 <chavanap96@gmail.com>
2025-03-08 21:06:57 +0530

    Try again

---
+++ StoreManagement/src/main/java/com/demo/model/Product.java
@@ -0,0 +1,35 @@
+package com.demo.model;
+
+public class Product {
+
+	private int productid;
+	private String prouctName;
+	private double price;
+	private String category;
+	public int getProductid() {
+		return productid;
+	}
+	public void setProductid(int productid) {
+		this.productid = productid;
+	}
+	public String getProuctName() {
+		return prouctName;
+	}
+	public void setProuctName(String prouctName) {
+		this.prouctName = prouctName;
+	}
+	public double getPrice() {
+		return price;
+	}
+	public void setPrice(double price) {
+		this.price = price;
+	}
+	public String getCategory() {
+		return category;
+	}
+	public void setCategory(String category) {
+		this.category = category;
+	}
+	
+	
+}
