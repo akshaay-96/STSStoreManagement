72de6ce3fc0c7bde11b27aa55c2405dc5eec1d5c
rockbandstyle123 <rockbandstyle123@gmail.com>
2025-03-08 21:30:57 +0530

    added POST API

--- StoreManagement/src/main/java/com/demo/controller/ProductController.java
+++ StoreManagement/src/main/java/com/demo/controller/ProductController.java
@@ -1,9 +1,20 @@
 package com.demo.controller;
 
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.demo.model.Product;
 
 @RestController 
 public class ProductController {
+	
+	
+	@PostMapping("/get")
+	public String saveData(@RequestBody Product p) {
+		
+		return "Data saved Successfully";
+	}
 
 	 
 }
